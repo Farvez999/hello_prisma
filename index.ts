6de6a849e0088a00930a2b9d4fbd542a6d7c7e7d
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-11 17:24:19 +0600

    Generating Prisma Client And Prisma Migrate

--- index.ts
+++ index.ts
@@ -3,16 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-    const getAllUsers = await prisma.user.findMany()
-    console.log(getAllUsers)
+    // const getAllUsers = await prisma.user.findMany()
+    // console.log(getAllUsers)
 
-    // const postUser = await prisma.user.create({
-    //     data: {
-    //         email: "firoz@gmail.com",
-    //         name: "Firoz"
-    //     }
-    // })
-    // console.log(postUser)
+    const postUser = await prisma.user.create({
+        data: {
+            email: "firoz@gmail.com",
+            name: "Firoz",
+            age: 34
+        }
+    })
+    console.log(postUser)
 }
 
 main();
